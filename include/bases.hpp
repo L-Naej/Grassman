822d2617c57709b8cbc44413e675f0b1b0028baf
L-Naej <jn.chiganne@gmail.com>
2013-12-30 11:11:25 +0100

    Definition des bases

---
+++ include/bases.hpp
@@ -0,0 +1,13 @@
+#ifndef BASES_HPP
+#define BASES_HPP
+
+///Les bases de P3
+struct Bases
+{
+  static const unsigned int e1 = 0x01;
+  static const unsigned int e2 = 0x02;
+  static const unsigned int e3 = 0x04;
+  static const unsigned int e4 = 0x08;
+};
+
+#endif // BASES_HPP
